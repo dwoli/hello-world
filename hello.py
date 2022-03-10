bf7ca0819e9f954606543961d6cb4af0d7f30e4e
dwoli <wolideependra@gmail.com>
2022-03-10 13:56:17 +0545

    python

---
+++ hello.py
@@ -0,0 +1,45 @@
+#output
+##print("hello world")
+#print("20")
+#print("welcome to the world of python")
+#input
+#name = input("what is your name?")
+#print("hello," +name+ "!")
+#type()
+#a=1
+#b=2.8
+#sum = a+b
+#print(sum)
+#print(type(sum))
+#a=2
+#print(type(a))
+#b=3.1
+#print(type(b))
+#c= "hello, world!"
+#print(type(c))
+#type conversions
+#a=2
+#b= float(a)
+##print(type(b))
+#a= "2"
+#print(type(a))
+#b= int(a)
+#print(b)
+#print(type(b))
+##operators
+from ast import Div, FloorDiv, Mod, Mult
+from operator import mod
+
+
+a=1
+b=2.8
+sum= a+b
+print(sum)
+Mult=a*b
+print(Mult)
+Div= a/b
+print(Div)
+Mod= a%b
+print(Mod)
+FloorDiv= a//b
+print(FloorDiv)

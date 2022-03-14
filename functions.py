a90c0c6c392a2b047858517fb5efa8c3540d8f29
dwoli <wolideependra@gmail.com>
2022-03-14 17:29:31 +0545

    functions

---
+++ functions.py
@@ -0,0 +1,58 @@
+#def greet(name):
+    #print("hello," + name)
+    #print("welcome to the world of python")
+#greet("deependra")
+#def address(city):
+ #   print("i live in " + city)
+#address("kathmandu")
+#def add(a,b):
+#    c=a+b
+#    return c
+#result=add(10,20)
+#print(result)
+#def mult(a,b):
+#    c=a*b
+#    return c
+#result= mult(2,3)
+#print(result)
+#def my_func(a,b=5):
+#    print(a,b)
+
+#my_func(3)
+#default arguments
+#def my_func(x,y=2):
+ #   print(x,y)
+#my_func(1)
+
+#keyword arguments
+#def my_func(a,b):
+ #   print(a,b)
+#my_func(1,2)
+
+#arbitary arguments
+#def my_func(*args):
+#    print(args)
+ #   print(type(args))
+#my_func(1,2)
+
+#kwargs
+#def my_func(**kwargs):
+#    print(kwargs)
+#my_func(a=1,b=2,c=3,d=4)
+
+#lambda functions
+#def sum(a,b):
+ #   return a+b
+
+#result=sum(3,5)
+#print(result)
+
+#sum = lambda a,b: a+b
+#print(sum(3,5))
+
+
+
+
+
+
+
